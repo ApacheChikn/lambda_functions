1a7c1ed88cefe263ded73656d453a242557d36eb
Zaire Ali <zaireali649@gmail.com>
2023-09-11 20:35:21 -0400

    Update lambda_function.py

--- s3/s3_trigger_get_key/lambda_function.py
+++ s3/s3_trigger_get_key/lambda_function.py
@@ -2,9 +2,16 @@ import json
 import boto3
 
 def lambda_handler(event, context):
+    # Print the event object for debugging purposes
+    print(event)
+    
+    # Extract the 'Records' field from the event
     records = event['Records']
     
+    # Iterate through the records
     for record in records:
+        # Extract the 'key' field from the S3 object in each record
         key = record['s3']['object']['key']
     
+        # Print the key value for each S3 object
         print(key)
